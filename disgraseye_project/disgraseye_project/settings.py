96336d207a02b4977063b974124b21e70ea6c137
RuiRui26 <rilgdinampo26@yahoo.com>
2026-03-04 12:41:47 +0800

    dsadasd

--- disgraseye_project/disgraseye_project/settings.py
+++ disgraseye_project/disgraseye_project/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_5%7=a4_&!g&(q*g2p9p$-egk3#l6lvoj!auaotckt83n76+a#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
